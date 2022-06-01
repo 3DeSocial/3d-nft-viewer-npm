8996d91bc0c6a1f185c78a709d7a4f0d769f027a
Andrew van Duivenbode <andrew@creativetransmissions.com>
2022-06-01 16:13:38 +0100

    WIP combined controls

---
+++ frontend/js/KeyboardControls.mjs
@@ -0,0 +1,9 @@
+let THREE;
+import { VOXMesh } from "three/examples/jsm/loaders/VOXLoader.js";
+export default class KeyboardControls {
+
+    constructor(config){
+        let defaults = {
+        }
+    }
+}
