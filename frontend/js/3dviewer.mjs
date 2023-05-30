412e9a6f9d9aac66f80f5a79af4037d0eb0a4d09
Andrew van Duivenbode <andrew@creativetransmissions.com>
2023-05-30 13:13:35 +0100

    switch to new VR method

--- frontend/js/3dviewer.mjs
+++ frontend/js/3dviewer.mjs
@@ -924,23 +924,111 @@ const params = {
         let vrBtnOptions = { btnCtr : 'div.view-vr-btn',
                              viewer: this,
                              onStartSession: ()=>{
-                                if(!this.player){
-                                    if(this.config.firstPerson){
-                                        this.initPlayerFirstPerson();
-                                    } else {
-                                        this.initPlayerThirdPerson();
-                                    };
-                                };
-                                console.log('start session walking');
-                                console.log('player rotation', this.player.rotation);
-                                console.log('camera.rotation', this.camera.rotation);
-                                console.log('character rotation',this.character.rotation);
-                                let vrType = that.getVrTypeFromUI();
-                                console.log('180 degrees later: ',that.camera.rotation);
+                                let vrType = 'walking';
 
-                                that.buildDolly(vrType);                                
+                                that.initVRSession(vrType);                                
                             } }
         let vrButtonEl = VRButton.createButton(this.renderer, vrBtnOptions);
+    }
+
+    initVRSession = (vrType) =>{
+        let that = this;
+
+        this.controlProxy = {};
+        this.vrControls = new VRControls({  renderer: this.renderer,
+                                            scene:this.scene,
+                                            vrType: 'walking',
+                                            moveUp: (data, value)=>{
+                                                that.controlProxy.data = data;
+                                                that.controlProxy.value = value;
+                                                that.controlProxy.dir = 'u';                                                
+                                            },
+                                            moveDown:(data, value)=>{
+                                                that.controlProxy.data = data;
+                                                that.controlProxy.value = value;
+                                                that.controlProxy.dir = 'd';
+                                            },
+                                            moveLeft:(data, value)=>{
+                                                console.log('moveLeft triggered');
+                                                that.controlProxy.data = data;
+                                                that.controlProxy.value = value;
+                                                that.controlProxy.dir = 'l';  
+                                            },                                            
+                                            moveRight:(data, value)=>{
+                                                console.log('moveright triggered');
+
+                                                that.controlProxy.data = data;
+                                                that.controlProxy.value = value;
+                                                that.controlProxy.dir = 'r';
+                                            },
+                                            moveForward:(data, value)=>{
+                                                that.controlProxy.data = data;
+                                                that.controlProxy.value = value;
+                                                that.controlProxy.dir = 'f';                                                   
+                                            },
+                                            moveBack:(data, value)=>{
+                                                that.controlProxy.data = data;
+                                                that.controlProxy.value = value;
+                                                that.controlProxy.dir = 'b';
+                                            },
+                                            rotateLeft: (data, value)=>{
+                                                that.controlProxy.data = data;
+                                                that.controlProxy.value = value;
+                                                that.controlProxy.rot = 'rl';
+                                            },
+                                            rotateRight: (data, value)=>{
+                                                that.controlProxy.data = data;
+                                                that.controlProxy.value = value;
+                                                that.controlProxy.rot = 'rr';
+                                            },
+                                            triggerLeft:(data, value)=>{
+                                            },
+                                            triggerRight:(data, value)=>{
+                                            },
+                                            paddleLeft:(data, value)=>{
+
+                                            },  
+                                            paddleRight:(data, value)=>{
+
+                                            },
+                                            stopMoving: ()=>{
+                                                that.controlProxy.data = null;
+                                                that.controlProxy.value = null;
+                                                that.controlProxy.dir =null;  
+                                            },
+                                            cancelRotate: ()=>{
+                                                that.controlProxy.isRotating = false;
+                                                that.controlProxy.rot = null;
+                                            },
+                                            onSelectStartLeft: (e,controller)=>{
+                                               console.log(controller.line);
+                                            },
+                                            onSelectEndLeft: (e,controller)=>{
+                                            },
+                                            onSelectStartRight: (e,controller)=>{
+                                               console.log(controller.line);
+                                               this.throwSnowBall(e,controller)
+
+                                            },
+                                            onSelectEndRight: (e,controller)=>{
+                                            }                                            
+                                        });
+
+        this.playerVR = new PlayerVR({  controllers: this.vrControls.controllers,
+                                        grips: this.vrControls.grips,
+                                        camera: this.camera,
+                                        controlProxy: this.controlProxy,
+                                        playerStartPos: this.player.position.clone(),
+                                        sceneCollider: this.sceneryLoader.collider});
+
+        this.scene.add(this.playerVR.dolly);
+        this.removePlayer();
+
+    }
+
+    removePlayer = () =>{
+        this.player.remove(...this.player.children);
+        this.scene.remove(this.player);
 
     }
 
@@ -1392,139 +1480,14 @@ initPlayerThirdPerson = () => {
     }
 
 }
-updatePlayerVR = (delta) =>{
-        this.playerVelocity.y += this.playerIsOnGround ? 0 : delta * params.gravity;
-        this.player.position.addScaledVector( this.playerVelocity, delta );
-          
-        if ( fwdPressed ) {
-            //this.tempVector.set( 0, 0, - 1 ).applyAxisAngle( this.upVector, angle );
-            this.player.translateZ(-params.playerSpeed * delta );
-        }
 
-        if ( bkdPressed ) {
-
-            //this.tempVector.set( 0, 0, 1 ).applyAxisAngle( this.upVector, angle );
-            this.player.translateZ(params.playerSpeed * delta );
-        }
-
-        if ( lftPressed ) {
-
-         //   this.tempVector.set( - 1, 0, 0 ).applyAxisAngle( this.upVector, angle );
-            this.player.translateX(-params.playerSpeed * delta );
-        }
-
-        if ( rgtPressed ) {
-
-           // this.tempVector.set( 1, 0, 0 ).applyAxisAngle( this.upVector, angle );
-            this.player.translateX(params.playerSpeed * delta );
-        }
-        this.player.updateMatrixWorld();
-
-        // adjust this.player position based on collisions
-        const capsuleInfo = this.character.capsuleInfo;
-        this.tempBox.makeEmpty();
-        this.tempMat.copy( this.collider.matrixWorld ).invert();
-        this.tempSegment.copy( capsuleInfo.segment );
-
-        // get the position of the capsule in the local space of the collider
-        this.tempSegment.start.applyMatrix4( this.player.matrixWorld ).applyMatrix4( this.tempMat );
-        this.tempSegment.end.applyMatrix4( this.player.matrixWorld ).applyMatrix4( this.tempMat );
-
-        // get the axis aligned bounding box of the capsule
-        this.tempBox.expandByPoint( this.tempSegment.start );
-        this.tempBox.expandByPoint( this.tempSegment.end );
-
-        this.tempBox.min.addScalar( - capsuleInfo.radius );
-        this.tempBox.max.addScalar( capsuleInfo.radius );
-
-        this.collider.geometry.boundsTree.shapecast( {
-
-            intersectsBounds: box => box.intersectsBox( this.tempBox ),
-
-            intersectsTriangle: tri => {
-
-                // check if the triangle is intersecting the capsule and adjust the
-                // capsule position if it is.
-                const triPoint = this.tempVector;
-                const capsulePoint = this.tempVector2;
-
-                const distance = tri.closestPointToSegment( this.tempSegment, triPoint, capsulePoint );
-                if ( distance < capsuleInfo.radius ) {
-
-                    const depth = capsuleInfo.radius - distance;
-                    const direction = capsulePoint.sub( triPoint ).normalize();
-
-                    this.tempSegment.start.addScaledVector( direction, depth );
-                    this.tempSegment.end.addScaledVector( direction, depth );
-
-                }
-
-            }
-
-        } );
-
-        // get the adjusted position of the capsule collider in world space after checking
-        // triangle collisions and moving it. capsuleInfo.segment.start is assumed to be
-        // the origin of the this.player model.
-        const newPosition = this.tempVector;
-        newPosition.copy( this.tempSegment.start ).applyMatrix4( this.collider.matrixWorld );
-
-        // check how much the collider was moved
-        const deltaVector = this.tempVector2;
-        deltaVector.subVectors( newPosition, this.player.position );
-
-        // if the this.player was primarily adjusted vertically we assume it's on something we should consider ground
-        this.playerIsOnGround = deltaVector.y > Math.abs( delta * this.playerVelocity.y * 0.25 );
-
-        const offset = Math.max( 0.0, deltaVector.length() - 1e-5 );
-        deltaVector.normalize().multiplyScalar( offset );
-
-        // adjust the this.player model
-        this.player.position.add( deltaVector );
-
-        if ( ! this.playerIsOnGround ) {
-
-            deltaVector.normalize();
-            if(this.showroomLoaded){
-               this.playerVelocity.addScaledVector( deltaVector, - deltaVector.dot( this.playerVelocity ) );
-            };
-
-        } else {
-
-            this.playerVelocity.set( 0, 0, 0 );
-
-        }
-        if(this.player.position){
-                
-            /* TO DO - fix this issue for avatar direction */
-            if(this.player.position.x){
-                    let playerx = this.player.position.x;
-                    let playery = this.player.position.y;
-                    let playerz = this.player.position.z;
-         
-                    this.dolly.position.set(playerx,(playery+0.5),playerz);
-                    this.dolly.rotation.copy(this.player.rotation);
-            }
+    updatePlayerVR = (delta) =>{
+        if(!this.playerVR){
+            return false;
         };
-        
 
-
-        // adjust the this.camerainit
-        this.camera.position.sub( this.controls.target );
-        this.controls.target.copy( this.player.position );
-        this.camera.position.add( this.player.position );
-
-        // if the this.player has fallen too far below the level reset their position to the start
-        if ( this.player.position.y < - 25 ) {
-
-            this.reset();
-
-        };
-        fwdPressed = false;
-        bkdPressed = false;
-        rgtPressed = false;
-        lftPressed = false;
-    }    
+        this.playerVR.moveDolly(delta);
+    }
 
     reset = ()=> {
         this.playerVelocity.set( 0, 0, 0 );
